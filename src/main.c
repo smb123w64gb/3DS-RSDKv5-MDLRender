045358e1aec9f235bdc4c5d96b4c8eb47341a7c7
smb123w64gb <smb123w64gb@gmail.com>
2022-10-03 21:11:40 -0700

    A working BUILD!

--- src/main.c
+++ src/main.c
@@ -31,6 +31,7 @@ u8* buffer;
 //this will have the vertex stride for each frame
 int* vStride;
 int vStrideCount;
+int vStrideCurrent = 0;
 int vertexSize;
 
 //3DS has VFPs so we could just use cos
@@ -123,7 +124,7 @@ struct gpuVertex { float position[3]; float normal[3]; float color[4];};
 
 void readMDL(){
 	int offsetCur = 0;
-	int sig = ReadInt32(&offsetCur);
+	offsetCur = 4;
 	char flags = ReadInt8(&offsetCur);
 
 	char faceVertCount = ReadInt8(&offsetCur);
@@ -148,10 +149,12 @@ void readMDL(){
             }
 		}
 	short indexCount = ReadInt16(&offsetCur);
+	printf("IndexCount:%i\n",indexCount);
 	short *indices = malloc(sizeof(short)*indexCount);
-	 for (int i = 0; i < indexCount; ++i) {
+	 for (int i = 0; i < indexCount; i++) {
 		indices[i] = ReadInt16(&offsetCur);
 	 }
+	 
 	 for (int f = 0; f < frameCount; ++f) {
             for (int v = 0; v < vertCount; ++v) {
                 vertices[(f * vertCount) + v].x = ReadSingle(&offsetCur);
@@ -168,26 +171,35 @@ void readMDL(){
                 }
             }
         }
+	printf("%i\n",offsetCur);
 	struct gpuVertex *Verts = malloc(sizeof(struct gpuVertex)*indexCount*frameCount);
 	vStride = malloc(frameCount*sizeof(int));
 	vStrideCount = frameCount;
 	vertexSize = (int)indexCount;
-	int curIdx = 0;
-	for(int f = 0;f<frameCount;f++){
-		vStride[f] = f*sizeof(struct gpuVertex)*indexCount;
-		for(int x = 0;x<indexCount; x++){
-			memcpy(&Verts[(f * vertCount) + x],&vertices[(f * vertCount) + indices[x]],sizeof(struct ModelVertex));
+	int y = 0;
+	for(int f = 0;f<frameCount;++f){
+		vStride[f] = f*indexCount;
+		for(int x = 0;x<vertexSize; ++x){
+			Verts[y].position[0] = vertices[(f * vertCount) + indices[x]].x;
+			Verts[y].position[1] = vertices[(f * vertCount) + indices[x]].y;
+			Verts[y].position[2] = vertices[(f * vertCount) + indices[x]].z;
+
+			Verts[y].normal[0] = vertices[(f * vertCount) + indices[x]].nx;
+			Verts[y].normal[1] = vertices[(f * vertCount) + indices[x]].ny;
+			Verts[y].normal[2] = vertices[(f * vertCount) + indices[x]].nz;
+			//printf("F:%i\tI:%i\tVert:%i\n",f,indices[x],(f * vertCount) + indices[x]);
 			if (flags & MODEL_USECOLOURS){
-			Verts[(f * vertCount) + x].color[0] =  (float)colors[indices[x]].bytes[0]/255.0f;
-			Verts[(f * vertCount) + x].color[1] =  (float)colors[indices[x]].bytes[1]/255.0f;
-			Verts[(f * vertCount) + x].color[2] =  (float)colors[indices[x]].bytes[2]/255.0f;
-			Verts[(f * vertCount) + x].color[3] =  (float)colors[indices[x]].bytes[3]/255.0f;
+			Verts[y].color[0] =  (float)colors[indices[x]].bytes[2]/255.0f;
+			Verts[y].color[1] =  (float)colors[indices[x]].bytes[1]/255.0f;
+			Verts[y].color[2] =  (float)colors[indices[x]].bytes[0]/255.0f;
+			Verts[y].color[3] =  (float)colors[indices[x]].bytes[3]/255.0f;
 			}else{
-				Verts[(f * vertCount) + x].color[0] = 1.0f;
-				Verts[(f * vertCount) + x].color[1] = 1.0f;
-				Verts[(f * vertCount) + x].color[2] = 1.0f;
-				Verts[(f * vertCount) + x].color[3] = 1.0f;
+				Verts[y].color[0] = 1.0f;
+				Verts[y].color[1] = 1.0f;
+				Verts[y].color[2] = 1.0f;
+				Verts[y].color[3] = 1.0f;
 			}
+			y++;
 		}
 	}
 	// Load the vertex shader, create a shader program and bind it
@@ -206,15 +218,14 @@ void readMDL(){
 	AttrInfo_AddLoader(attrInfo, 0, GPU_FLOAT, 3); // v0=position
 	AttrInfo_AddLoader(attrInfo, 1, GPU_FLOAT, 3); // v1=normal
 	AttrInfo_AddLoader(attrInfo, 2,  GPU_FLOAT, 4); // v2=color
-
 	// Create the VBO (vertex buffer object)
 	int allocation = sizeof(struct gpuVertex)*indexCount*frameCount;
 	vbo_data = linearAlloc(allocation);
 	
 	memcpy(vbo_data, Verts, allocation);
-
-	printf("%f",Verts[0].position[1]);
-	printf("%f",Verts[0].position[2]);
+	free(Verts);
+	//printf("%f",Verts[0].position[1]);
+	//printf("%f",Verts[0].position[2]);
 	// Configure buffers
 	C3D_BufInfo* bufInfo = C3D_GetBufInfo();
 	BufInfo_Init(bufInfo);
@@ -222,20 +233,21 @@ void readMDL(){
 
 	C3D_TexEnv* env = C3D_GetTexEnv(0);
 	C3D_TexEnvInit(env);
-	C3D_TexEnvSrc(env, C3D_Both, GPU_FRAGMENT_PRIMARY_COLOR, GPU_FRAGMENT_SECONDARY_COLOR, 0);
-	C3D_TexEnvFunc(env, C3D_Both, GPU_ADD);
+	C3D_TexEnvSrc(env, C3D_Both, GPU_PRIMARY_COLOR, 0, 0);
+	C3D_TexEnvFunc(env, C3D_Both, GPU_REPLACE);
 
-
+printf("%i\n",vertexSize);
 	
 }
 
-static void sceneRender(void)
+static void sceneRender(float iod)
 {
+	C3D_CullFace(GPU_CULL_NONE);
+	Mtx_PerspStereoTilt(&projection, C3D_AngleFromDegrees(40.0f), C3D_AspectRatioTop, 0.01f, 1000.0f, iod, 2.0f, false);
 	// Calculate the modelView matrix
 	C3D_Mtx modelView;
 	Mtx_Identity(&modelView);
-	Mtx_Translate(&modelView, 0.0, 35.0f, 175.0f, true);
-	Mtx_RotateX(&modelView, angleX, true);
+	Mtx_Translate(&modelView, 0.0, -35.0f, -175.0f, true);
 	Mtx_RotateY(&modelView, angleY, true);
 
 	// Rotate the cube each frame
@@ -247,21 +259,24 @@ static void sceneRender(void)
 	C3D_FVUnifMtx4x4(GPU_VERTEX_SHADER, uLoc_modelView,  &modelView);
 
 	// Draw the VBO
-	//printf("%i\n",vertexSize);
+	
 	//printf("%i\n",vertexSize/3);
-	C3D_DrawArrays(GPU_TRIANGLES, 0, vertexSize/3);
+	C3D_DrawArrays(GPU_TRIANGLES, vStride[vStrideCurrent], vertexSize);
 }
 
 int main(int argc, char** argv)
 {
 
 	gfxInitDefault(); //makes displaying to screen easier
+	gfxSet3D(true);
 	C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
 
-	C3D_RenderTarget* target = C3D_RenderTargetCreate(240, 400, GPU_RB_RGBA8, GPU_RB_DEPTH24_STENCIL8);
-	C3D_RenderTargetSetOutput(target, GFX_TOP, GFX_LEFT, DISPLAY_TRANSFER_FLAGS);
+	C3D_RenderTarget* targetLeft  = C3D_RenderTargetCreate(240, 400, GPU_RB_RGBA8, GPU_RB_DEPTH24_STENCIL8);
+	C3D_RenderTarget* targetRight = C3D_RenderTargetCreate(240, 400, GPU_RB_RGBA8, GPU_RB_DEPTH24_STENCIL8);
+	C3D_RenderTargetSetOutput(targetLeft,  GFX_TOP, GFX_LEFT,  DISPLAY_TRANSFER_FLAGS);
+	C3D_RenderTargetSetOutput(targetRight, GFX_TOP, GFX_RIGHT, DISPLAY_TRANSFER_FLAGS);
 
-	FILE *file = fopen("UFOChase.bin","rb");
+	FILE *file = fopen("SonicJog.bin","rb");
 	if (file == NULL) goto exit;
 
 	// seek to end of file
@@ -278,7 +293,7 @@ int main(int argc, char** argv)
 	if(!buffer)goto exit;
 
 	//read contents !
-	off_t bytesRead = fread(buffer,1,size,file);
+	fread(buffer,1,size,file);
 
 	//close the file because we like being nice and tidy
 	fclose(file);
@@ -294,14 +309,27 @@ int main(int argc, char** argv)
 		hidScanInput();
 		if(keysHeld()&KEY_B)break;
 
+		float slider = osGet3DSliderState();
+		float iod = slider/3;
+
 
 		C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
 		{
-			C3D_RenderTargetClear(target, C3D_CLEAR_ALL, 0x68B0D8FF, 0);
-			C3D_FrameDrawOn(target);
-			sceneRender();
+
+			C3D_RenderTargetClear(targetLeft, C3D_CLEAR_ALL, 0x68B0D8FF, 0);
+			C3D_FrameDrawOn(targetLeft);
+			sceneRender(-iod);
+
+			if (iod > 0.0f)
+			{
+				C3D_RenderTargetClear(targetRight, C3D_CLEAR_ALL, 0x68B0D8FF, 0);
+				C3D_FrameDrawOn(targetRight);
+				sceneRender(iod);
+			}
 		}
 		C3D_FrameEnd(0);
+		vStrideCurrent++;
+		if(vStrideCurrent>=vStrideCount)vStrideCurrent=0;
 		//gspWaitForEvent(GSPGPU_EVENT_VBlank0, false);
 	}
 
@@ -311,6 +339,7 @@ int main(int argc, char** argv)
 
 	//closing all services even more so
 	linearFree(vbo_data);
+	free(buffer);
 	shaderProgramFree(&program);
 	DVLB_Free(vshader_dvlb);
 	C3D_Fini();
